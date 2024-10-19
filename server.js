be7745bade3cb02d518edc7bbb2f92a2bbc23081
Artifice-219 <johnphilliplormalbas@gmail.com>
2024-10-19 17:20:31 +0800

    corrected a routing path in server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// TODO 1 : does this path even works ?
 const connectDB = require('./backend/config/db/dbConn.js');  // Database connection
 // TODO 2 : this route is not created as of yet
 const userRoutes = require('./routes/userRoutes');  // Routes
@@ -9,15 +8,14 @@ const port = 3000;
 
 // TODO 3 : THIS IS AN ERRONOUS EXPORT FILE
 // Connect to MongoDB
+// TODO 9 : AS THIS IS AN EXPORT FUNCTION HAVE A TRY CATCH BLOCK HERE TO MAKE SURE THAT ALL IS WORKING
 main()
 
 // Middleware to parse form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static HTML files from the "public" directory
-// TODO: THERE IS NO SUCH THING AS 'PUBLIC' ON YOURE DIRECTORY GALING PA TO SA PRACTICE FILE MO E
-app.use(express.static('public'));
+app.use(express.static('frontend/pages'));
 
 // Routes for handling form submission
 app.use('/api', userRoutes);  // API routes
