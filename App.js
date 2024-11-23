e9be469e1b5a19472b223fde61faec9c1731fac6
Artifice-219 <johnphilliplormalbas@gmail.com>
2024-11-23 05:13:24 -0800

    added an env for the database credentials, some routes

--- App.js
+++ App.js
@@ -3,12 +3,24 @@ const app = express();
 const path = require('path')
 
 // defining route for the pages
-app.use('pages',express.static(__dirname + '/frontend'))
+// 'pages' here is a virtual path
+app.use('pages',express.static(__dirname + 'frontend'))
+// another static route to served the sign/login shit from the SignIn&SignUp folder
+// app.use(express.static(path.join(__dirname , 'SignIn&SignUp')))
+// root leve middleware logger 
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${__dirname + req.url}`);
+    next();
+})
 // serving the index.html
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'index.html'))
 })
+
+// serving the login/signup page
+app.get('/SignIn&SignUp/SignUp.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'SignIn&SignUp', 'SignUp.html'));
+  });
 
 
 
