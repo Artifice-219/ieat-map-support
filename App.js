ae20652db23c19a0ff540b380f871fabbb9692c8
Artifice-219 <johnphilliplormalbas@gmail.com>
2024-12-04 05:24:18 -0800

    middleware functions & housekeeping

--- App.js
+++ App.js
@@ -2,23 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const session = require('express-session')
-// this shit is needed to parse post request
 const bodyParser = require("body-parser");
 const Student = require("./backend/model/studentModel.js");
+const conn = require('./backend/config/dbconn.js')
+const saveStudent = require('./backend/controller/database/saveStudent.js')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// defining route for the pages
-// 'pages' here is a virtual path
-// TODO 22 : WHATEVER IS THE SIGNIFICANCE OF THIS STATIC SHIT HERE
 app.use("/static", express.static(__dirname + "/frontend"));
-// another static route to served the sign/login shit from the SignIn&SignUp folder
-// app.use(express.static(path.join(__dirname , 'SignIn&SignUp')))
 
-// session fucking middleware
-// use this shit to store logged in user data
+
 app.use(session({
-  // TODO 27 : REPLACE THIS FUCKER ONCE EVERTHING IS GOODS AND DONE
+  // TODO 41 : REPLACE THIS FUCKER ONCE EVERTHING IS GOODS AND DONE
   secret : 'fuck_me',
   resave : false,
   saveUninitialized: true, 
@@ -29,29 +24,21 @@ app.use((req, res, next) => {
   console.log(`${req.method} request made to ${__dirname + req.url}`);
   next();
 });
-// serving the index.html
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// serving the login/signup page
 app.get("/SignIn&SignUp/SignUp.html", (req, res) => {
   res.sendFile(path.join(__dirname, "SignIn&SignUp", "SignUp.html"));
 });
 
-// login route
 app.post("/login",async (req, res, next) => {
-    // TODO 26 : THIS WORKS BUT YOU ARE NOT UTILIZING THE RETURN DATA FROM THE DB, IT SHOULD BE USE TO FILL IN THE DATA IN DASHBOARD
   const email = req.body.email;
   const password = req.body.password;
 
-  const mongoose = require("mongoose");
-  const mongo_uri ="mongodb+srv://johnphillipmalbasdev:JA7RY5uorElI2cYg@cluster0.9pms9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-  mongoose.connect(mongo_uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  conn()
 
   try{
     const studentFound = await Student.findOne({email : email});
@@ -62,8 +49,8 @@ app.post("/login",async (req, res, next) => {
         })
     }
    
-    // store that fucking email on the session thing
     req.session.email = email;
+    
   }catch(err){
     console.error(`Error finding student ${err}`);
     res.status(500).send({
@@ -74,11 +61,11 @@ app.post("/login",async (req, res, next) => {
   next();
 },
 (req, res) => {
-    // render a dashboard
+
     res.sendFile(path.join(__dirname, "frontend/pages/dashboard/studDashboard.html"))
 });
 
-// signup route
+
 app.post("/signup", async (req, res, next) => {
   const username = req.body.username;
   const email = req.body.email;
@@ -86,33 +73,10 @@ app.post("/signup", async (req, res, next) => {
   const age = req.body.age;
   const studentNumber = req.body.number;
   const password = req.body.password;
-    // TODO 23 : HAVE THIS CONNECTION LOGIC ON ITS FILE PARA DI MAGULO
-  const mongoose = require("mongoose");
-  const mongo_uri ="mongodb+srv://johnphillipmalbasdev:JA7RY5uorElI2cYg@cluster0.9pms9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-  mongoose.connect(mongo_uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-//   TODO 24 : HAVE THIS LOGIC ON ITS FILE PARA DI MAGULO   
+  conn()
   // saved all this shit to the database
-  try {
-    const newStudent = new Student({
-      firstName: username,
-      lastName: "you have no last name param",
-      email : email,
-      age: age,
-      gender: gender,
-      studentNumber: studentNumber,
-      password : password
-    });
-
-    const savedStudent = await newStudent.save();
-    // res.json(savedStudent)
-  } catch (err) {
-    console.error(`Cannot save student :  ${err}`);
-  }
+  saveStudent(username, email, age, gender, studentNumber, password)
   next();
 },(req, res) =>{
     res.sendFile(path.join(__dirname, "frontend/pages/dashboard/studDashboard.html"))
@@ -123,14 +87,7 @@ app.post("/signup", async (req, res, next) => {
 app.get('/user', async (req, res ) => {
   let email = req.session.email;
 
-      // TODO 23 : HAVE THIS CONNECTION LOGIC ON ITS FILE PARA DI MAGULO
-      const mongoose = require("mongoose");
-      const mongo_uri ="mongodb+srv://johnphillipmalbasdev:JA7RY5uorElI2cYg@cluster0.9pms9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    
-      mongoose.connect(mongo_uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      conn()
 
       // query the database using the email on the route param
       try{
--- backend/config/dbconn.js
+++ backend/config/dbconn.js
@@ -1,4 +1,29 @@
-const mongoose = require('mongoose');
-const mongo_uri = ' mongodb+srv://johnphillipmalbasdev:JA7RY5uorElI2cYg@cluster0.9pms9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoose = require("mongoose");
+const mongo_uri ="mongodb+srv://johnphillipmalbasdev:JA7RY5uorElI2cYg@cluster0.9pms9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(mongo_uri, {useNewUrlParser : true, useUnifiedTopology : true});
+async function conn(){
+
+    try{
+
+       await  mongoose.connect(mongo_uri, {useNewUrlParser : true, useUnifiedTopology : true});
+       console.log('From dbconn : Successfully connected to database')
+    }
+    catch(e){
+        console.error(`'Stupid programmer ' ${e}`)
+    }
+    
+}
+
+module.exports =  conn;
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// const mongo_uri = ' mongodb+srv://johnphillipmalbasdev:JA7RY5uorElI2cYg@cluster0.9pms9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// mongoose.connect(mongo_uri, {useNewUrlParser : true, useUnifiedTopology : true});
